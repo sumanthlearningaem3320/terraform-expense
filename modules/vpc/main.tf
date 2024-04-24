10ff0cc88c41a00405dc710a7bfbd635a258566c
sumanthlearningaem3320 <saisumanthguntur@gmail.com>
2024-04-24 09:35:18 +0530

    creating 8 subnets

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,26 +12,26 @@ resource "aws_subnet" "public" {
   availability_zone = var.azs[count.index]
 
 }
-#resource "aws_subnet" "web" {
-#  vpc_id            = aws_vpc.main.id
-#  count = length(var.public_subnets)
-#  cidr_block        = var.public_subnets[count.index]
-#  tags = merge(var.tags, {Name = "vpc-web-subnets"})
-#  availability_zone = var.azs[count.index]
-#}
-#resource "aws_subnet" "app" {
-#  vpc_id            = aws_vpc.main.id
-#  count = length(var.public_subnets)
-#  cidr_block        = var.public_subnets[count.index]
-#  tags = merge(var.tags, {Name = "vpc-app-subnets"})
-#  availability_zone = var.azs[count.index]
-#
-#}
-#resource "aws_subnet" "db" {
-#  vpc_id            = aws_vpc.main.id
-#  count = length(var.public_subnets)
-#  cidr_block        = var.public_subnets[count.index]
-#  tags = merge(var.tags, {Name = "vpc-db-subnets"})
-#  availability_zone = var.azs[count.index]
-#
-#}
+resource "aws_subnet" "web" {
+  vpc_id            = aws_vpc.main.id
+  count = length(var.public_subnets)
+  cidr_block        = var.public_subnets[count.index]
+  tags = merge(var.tags, {Name = "vpc-web-subnets"})
+  availability_zone = var.azs[count.index]
+}
+resource "aws_subnet" "app" {
+  vpc_id            = aws_vpc.main.id
+  count = length(var.public_subnets)
+  cidr_block        = var.public_subnets[count.index]
+  tags = merge(var.tags, {Name = "vpc-app-subnets"})
+  availability_zone = var.azs[count.index]
+
+}
+resource "aws_subnet" "db" {
+  vpc_id            = aws_vpc.main.id
+  count = length(var.public_subnets)
+  cidr_block        = var.public_subnets[count.index]
+  tags = merge(var.tags, {Name = "vpc-db-subnets"})
+  availability_zone = var.azs[count.index]
+
+}
